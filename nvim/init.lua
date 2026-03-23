5c890b3b385d4a7937ecf4154dd90eec8c3199a4
Duy Hoa Tran <hoatranduy@gmail.com>
2026-03-23 09:59:30 +0100

    Separate treesitter handles

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,7 @@
 require("settings")
 require("ui")
 require("plugins")
+require("treesitter")
 require("mappings")
 require("autocmds")
 require("lsp")
---
+++ nvim/lua/treesitter.lua
@@ -0,0 +1,41 @@
+-- lua/treesitter.lua
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = {
+    "bash",
+    "lua",
+    "vim",
+    "c",
+    "cpp",
+    "diff",
+    "cmake",
+    "git_config",
+    "git_rebase",
+    "gitattributes",
+    "gitcommit",
+    "gitignore",
+    "ssh_config",
+    "csv",
+    "json",
+    "python",
+    "pascal",
+    "comment",
+    "markdown",
+    -- "cuda",
+    -- "dockerfile"
+    -- "make",
+    -- "regex",
+    -- "sql",
+  },
+  callback = function(args)
+    local ft = args.match
+    local lang = vim.treesitter.language.get_lang(ft)
+    if lang and vim.treesitter.language.add(lang) then
+        vim.treesitter.start()
+        vim.wo.foldmethod = 'expr'
+        vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end
+  end,
+})
+
