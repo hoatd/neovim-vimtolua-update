98f19c367f59d7c924a1df30c6920f76d2a17bd1
Duy Hoa Tran <hoatranduy@gmail.com>
2026-03-31 13:08:27 +0200

    Gate using treesitter and logic in autcmd

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -2,14 +2,6 @@
 
 local M = {}
 
-local utils = require("utils")
-local ts = require("nvim-treesitter")
-local ts_textobjects = require("nvim-treesitter-textobjects")
-local ts_move = require("nvim-treesitter-textobjects.move")
-local ts_swap = require("nvim-treesitter-textobjects.swap")
-local ts_select = require("nvim-treesitter-textobjects.select")
-local ts_context = require("treesitter-context")
-
 local parsers = {
   "bash",
   "c",
@@ -54,91 +46,277 @@ local function setup_keymaps(bufnr)
   -- ============================================================
   -- Treesitter textobjects movements
   -- ============================================================
+  local ok_ts_textobjects, ts_textobjects =
+    pcall(require, "nvim-treesitter-textobjects")
+  if not ok_ts_textobjects then
+    return
+  end
 
   -- Function / Class / Parameter / Smart text objects navigation
-  map("n", "]f", function()
-    ts_move.goto_next_start("@function.outer", "textobjects")
-  end, vim.tbl_extend("force", opts, { desc = "Jump to next function start" }))
+  local ok_ts_move, ts_move = pcall(require, "nvim-treesitter-textobjects.move")
+  if not ok_ts_move then
+    vim.notify(
+      "Treesitter: nvim-treesitter-textobjects.move not available"
+        .. (ts_move or "unknown error"),
+      vim.log.levels.WARN
+    )
+  else
+    map(
+      "n",
+      "]f",
+      function()
+        ts_move.goto_next_start("@function.outer", "textobjects")
+      end,
+      vim.tbl_extend("force", opts, { desc = "Jump to next function start" })
+    )
+    map(
+      "n",
+      "[f",
+      function()
+        ts_move.goto_previous_start("@function.outer", "textobjects")
+      end,
+      vim.tbl_extend(
+        "force",
+        opts,
+        { desc = "Jump to previous function start" }
+      )
+    )
+    map("n", "]c", function()
+      ts_move.goto_next_start("@class.outer", "textobjects")
+    end, vim.tbl_extend(
+      "force",
+      opts,
+      { desc = "Jump to next class start" }
+    ))
+    map(
+      "n",
+      "[c",
+      function()
+        ts_move.goto_previous_start("@class.outer", "textobjects")
+      end,
+      vim.tbl_extend("force", opts, { desc = "Jump to previous class start" })
+    )
+    map("n", "]a", function()
+      ts_move.goto_next_start("@parameter.inner", "textobjects")
+    end, vim.tbl_extend("force", opts, { desc = "Jump to next parameter" }))
+    map("n", "[a", function()
+      ts_move.goto_previous_start("@parameter.inner", "textobjects")
+    end, vim.tbl_extend(
+      "force",
+      opts,
+      { desc = "Jump to previous parameter" }
+    ))
+  end
 
-  map(
-    "n",
-    "[f",
-    function()
-      ts_move.goto_previous_start("@function.outer", "textobjects")
-    end,
-    vim.tbl_extend("force", opts, { desc = "Jump to previous function start" })
-  )
+  -- Function / Class / Parameter / Smart text objects selection
+  local ok_ts_select, ts_select =
+    pcall(require, "nvim-treesitter-textobjects.select")
+  if not ok_ts_select then
+    vim.notify(
+      "Treesitter: nvim-treesitter-textobjects.select not available"
+        .. (ts_select or "unknown error"),
+      vim.log.levels.WARN
+    )
+  else
+    map({ "x", "o" }, "af", function()
+      ts_select.select_textobject("@function.outer", "textobjects")
+    end, vim.tbl_extend("force", opts, { desc = "Select around function" }))
 
-  map("n", "]c", function()
-    ts_move.goto_next_start("@class.outer", "textobjects")
-  end, vim.tbl_extend("force", opts, { desc = "Jump to next class start" }))
+    map({ "x", "o" }, "if", function()
+      ts_select.select_textobject("@function.inner", "textobjects")
+    end, vim.tbl_extend("force", opts, { desc = "Select inside function" }))
 
-  map("n", "[c", function()
-    ts_move.goto_previous_start("@class.outer", "textobjects")
-  end, vim.tbl_extend(
-    "force",
-    opts,
-    { desc = "Jump to previous class start" }
-  ))
+    map({ "x", "o" }, "ac", function()
+      ts_select.select_textobject("@class.outer", "textobjects")
+    end, vim.tbl_extend("force", opts, { desc = "Select around class" }))
 
-  map("n", "]a", function()
-    ts_move.goto_next_start("@parameter.inner", "textobjects")
-  end, vim.tbl_extend("force", opts, { desc = "Jump to next parameter" }))
+    map({ "x", "o" }, "ic", function()
+      ts_select.select_textobject("@class.inner", "textobjects")
+    end, vim.tbl_extend("force", opts, { desc = "Select inside class" }))
 
-  map("n", "[a", function()
-    ts_move.goto_previous_start("@parameter.inner", "textobjects")
-  end, vim.tbl_extend("force", opts, { desc = "Jump to previous parameter" }))
+    map({ "x", "o" }, "aa", function()
+      ts_select.select_textobject("@parameter.outer", "textobjects")
+    end, vim.tbl_extend("force", opts, { desc = "Select around parameter" }))
 
-  map({ "x", "o" }, "af", function()
-    ts_select.select_textobject("@function.outer", "textobjects")
-  end, vim.tbl_extend("force", opts, { desc = "Select around function" }))
+    map({ "x", "o" }, "ia", function()
+      ts_select.select_textobject("@parameter.inner", "textobjects")
+    end, vim.tbl_extend("force", opts, { desc = "Select inside parameter" }))
+  end
 
-  map({ "x", "o" }, "if", function()
-    ts_select.select_textobject("@function.inner", "textobjects")
-  end, vim.tbl_extend("force", opts, { desc = "Select inside function" }))
+  -- Function / Class / Parameter / Smart text objects swap
+  local ok_ts_swap, ts_swap = pcall(require, "nvim-treesitter-textobjects.swap")
+  if not ok_ts_swap then
+    vim.notify(
+      "Treesitter: nvim-treesitter-textobjects.swap not available"
+        .. (ts_swap or "unknown error"),
+      vim.log.levels.WARN
+    )
+  else
+    map("n", "<leader>a", function()
+      ts_swap.swap_next("@parameter.inner")
+    end, vim.tbl_extend("force", opts, { desc = "Swap next node" }))
 
-  map({ "x", "o" }, "ac", function()
-    ts_select.select_textobject("@class.outer", "textobjects")
-  end, vim.tbl_extend("force", opts, { desc = "Select around class" }))
-
-  map({ "x", "o" }, "ic", function()
-    ts_select.select_textobject("@class.inner", "textobjects")
-  end, vim.tbl_extend("force", opts, { desc = "Select inside class" }))
-
-  map({ "x", "o" }, "aa", function()
-    ts_select.select_textobject("@parameter.outer", "textobjects")
-  end, vim.tbl_extend("force", opts, { desc = "Select around parameter" }))
-
-  map({ "x", "o" }, "ia", function()
-    ts_select.select_textobject("@parameter.inner", "textobjects")
-  end, vim.tbl_extend("force", opts, { desc = "Select inside parameter" }))
-
-  map("n", "<leader>a", function()
-    ts_swap.swap_next("@parameter.inner")
-  end, vim.tbl_extend("force", opts, { desc = "Swap next node" }))
-
-  map("n", "<leader>A", function()
-    ts_swap.swap_previous("@parameter.outer")
-  end, vim.tbl_extend("force", opts, { desc = "Swap previous node" }))
+    map("n", "<leader>A", function()
+      ts_swap.swap_previous("@parameter.outer")
+    end, vim.tbl_extend("force", opts, { desc = "Swap previous node" }))
+  end
 end
 
 function M.setup()
+  -- ============================================================
+  -- nvim-treesitter setup
+  -- ============================================================
+  local ok_treesitter, treesitter = pcall(require, "nvim-treesitter")
+  if not ok_treesitter then
+    vim.notify(
+      "Plugin: nvim-treesitter failed setting up: "
+        .. (treesitter or "unknown error"),
+      vim.log.levels.ERROR
+    )
+    return
+  end
   -- Install parsers automatically
-  ts.install(parsers)
+  treesitter.install(parsers)
+  -- treesitter.setup({}) -- Dont need setup by default
 
-  -- Dont need setup by default
-  -- ts.setup({})
+  -- ============================================================
+  -- nvim-treesitter-textobjects setup
+  -- ============================================================
+  local ok_ts_textobjects, ts_textobjects =
+    pcall(require, "nvim-treesitter-textobjects")
+  if not ok_ts_textobjects then
+    vim.notify(
+      "Plugin: nvim-treesitter-textobjects failed setting up: "
+        .. (ts_textobjects or "unknown error"),
+      vim.log.levels.WARN
+    )
+  else
+    ts_textobjects.setup({
+      select = {
+        enable = true,
+        lookahead = true,
+      },
+
+      move = {
+        enable = true,
+        set_jumps = true,
+      },
+
+      swap = {
+        enable = true,
+      },
+    })
+  end
+
+  -- ============================================================
+  -- nvim-treesitter-context setup
+  -- ============================================================
+  local ok_ts_context, ts_context = pcall(require, "treesitter-context")
+  if not ok_ts_context then
+    vim.notify(
+      "Plugin: nvim-treesitter-context failed setting up: "
+        .. (ts_context or "unknown error"),
+      vim.log.levels.WARN
+    )
+  else
+    ts_context.setup({})
+  end
+
+  local utils = require("utils")
 
   -- ============================================================
   -- Per-Filetype setup
   -- ============================================================
+  local LARGE_FILE_SIZE_LIMIT = 5 * 1024 * 1024
+  local LONG_FILE_LINES_LIMIT = 20000
+
+  -- ============================================================
+  -- Large (>5 MB) or Long (20k lines) file optimizations
+  -- ============================================================
+  vim.api.nvim_create_autocmd("BufReadPre", {
+    group = utils.augroup("large_file"),
+    callback = function(args)
+      local buf = args.buf
+
+      local name = vim.api.nvim_buf_get_name(buf)
+      -- Early exit for empty / unnamed buffer
+      if name == "" then
+        return
+      end
+
+      -- Early exit for non file buffer: terminal, help, ...
+      local ok, stat = pcall(vim.uv.fs_stat, name)
+      if not ok or not stat or stat.type ~= "file" then
+        return
+      end
+
+      -- Early exit for small files
+      local size = stat.size
+      if size <= LARGE_FILE_SIZE_LIMIT then
+        return
+      end
+
+      -- Mark this buffer as large file
+      vim.b[buf].large_file = true
+
+      vim.bo[buf].swapfile = false
+      vim.bo[buf].undolevels = -1
+
+      -- Large file → Performance mode disable swap and undo
+      vim.notify(
+        string.format(
+          "Large file (%.1f MB) detected: " .. "No swap, no undo support.",
+          size / (1024 * 1024)
+        ),
+        vim.log.levels.WARN
+      )
+    end,
+  })
+
   vim.api.nvim_create_autocmd({ "FileType", "BufReadPost" }, {
     group = utils.augroup("treesitter"),
     callback = function(args)
       local buf = args.buf
-      local ft = vim.bo[buf].filetype or ""
 
+      local ft = vim.bo[buf].filetype or ""
+      -- Early exit for non filetype buffer
       if ft == "" then
+        return
+      end
+
+      local name = vim.api.nvim_buf_get_name(buf)
+      -- Early exit for empty / unnamed buffer
+      if name == "" then
+        return
+      end
+
+      -- Skip if this buffer is in performance mode
+      -- as it has been check in BufReadPre autocmds
+      if vim.b[buf].large_file then
+        return
+      end
+
+      local line_count = vim.api.nvim_buf_line_count(buf)
+      if line_count > LONG_FILE_LINES_LIMIT then
+        -- Very long file → Full performance mode
+        vim.wo.foldmethod = "manual"
+        vim.wo.foldenable = false
+        vim.wo.spell = false
+        vim.wo.wrap = false
+        vim.wo.cursorline = false
+        vim.wo.cursorcolumn = false
+        vim.wo.signcolumn = "no"
+        vim.wo.relativenumber = false
+        vim.wo.number = false
+
+        vim.notify(
+          string.format(
+            "Very long file (%dK lines) detected: "
+              .. "Treesitter features are disable.",
+            math.floor(line_count / 1000)
+          ),
+          vim.log.levels.WARN
+        )
         return
       end
 
@@ -162,11 +340,6 @@ function M.setup()
             .. (lang_add_err or "unknown error"),
           vim.log.levels.WARN
         )
-        return
-      end
-
-      -- Skip if this buffer is in performance mode
-      if vim.b[buf].large_file then
         return
       end
 
@@ -199,106 +372,6 @@ function M.setup()
       )
     end,
   })
-
-  -- ============================================================
-  -- Large (>5 MB) or Long (20k lines) file optimizations
-  -- ============================================================
-  local LARGE_FILE_SIZE = 5 * 1024 * 1024
-  local LONG_FILE_LINES = 20000
-  vim.api.nvim_create_autocmd("BufReadPre", {
-    group = utils.augroup("large_file"),
-    callback = function(args)
-      local buf = args.buf
-      local name = vim.api.nvim_buf_get_name(buf)
-
-      -- Early exit for empty / unnamed buffer
-      if name == "" then
-        return
-      end
-
-      -- Early exit for non file buffer: terminal, help, ...
-      local ok, stat = pcall(vim.uv.fs_stat, name)
-      if not ok or not stat or stat.type ~= "file" then
-        return
-      end
-
-      -- Early exit for small files
-      local size = stat.size
-      if size <= LARGE_FILE_SIZE then
-        return
-      end
-
-      -- Mark this buffer as large file
-      vim.b[buf].large_file = true
-
-      vim.bo[buf].swapfile = false
-      vim.bo[buf].undolevels = -1
-
-      local line_count = vim.api.nvim_buf_line_count(buf)
-      if line_count > LONG_FILE_LINES then
-        -- Very long file → Full performance mode
-        vim.wo.foldmethod = "manual"
-        vim.wo.foldenable = false
-        vim.wo.spell = false
-        vim.wo.wrap = false
-        vim.wo.cursorline = false
-        vim.wo.cursorcolumn = false
-        vim.wo.signcolumn = "no"
-        vim.wo.relativenumber = false
-        vim.wo.number = false
-
-        pcall(vim.treesitter.stop, buf)
-
-        vim.notify(
-          string.format(
-            "Treesitter: Very long file (%dK lines, %.1f MB) detected "
-              .. "→ Full performance mode enabled: "
-              .. "No swap, no undo, no spell, no wrap, no line visualization. "
-              .. "Treesitter features are disable as well.",
-            math.floor(line_count / 1000),
-            size / (1024 * 1024)
-          ),
-          vim.log.levels.WARN
-        )
-      else
-        -- Medium-large file → Light performance mode only disable swap and undo (keep highlighting,
-        -- indentation, folding, spell, wrap, and line visualization)
-        vim.notify(
-          string.format(
-            "Treesitter: Large file (%.1f MB) detected "
-              .. "→ Light performance mode enabled: "
-              .. "No swap, no undo support.",
-            size / (1024 * 1024)
-          ),
-          vim.log.levels.WARN
-        )
-      end
-    end,
-  })
-
-  -- ============================================================
-  -- nvim-treesitter-textobjects setup
-  -- ============================================================
-  ts_textobjects.setup({
-    select = {
-      enable = true,
-      lookahead = true,
-    },
-
-    move = {
-      enable = true,
-      set_jumps = true,
-    },
-
-    swap = {
-      enable = true,
-    },
-  })
-
-  -- ============================================================
-  -- nvim-treesitter-context setup
-  -- ============================================================
-  ts_context.setup({})
 end
 
 return M
