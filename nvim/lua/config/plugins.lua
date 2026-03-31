0a2fd0c5a65bdffb8a5d2a336450c7314dc854c2
Duy Hoa Tran <hoatranduy@gmail.com>
2026-03-31 14:11:46 +0200

    Remove commented setup options

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -173,24 +173,6 @@ function M.setup()
   if ok_tabby then
     tabby.setup({
       preset = "active_wins_at_tail",
-      --  option = {
-      --    theme = {
-      --      fill = 'TabLineFill',       -- tabline background
-      --      head = 'TabLine',           -- head element highlight
-      --      current_tab = 'TabLineSel', -- current tab label highlight
-      --      tab = 'TabLine',            -- other tab label highlight
-      --      win = 'TabLineSel',            -- window highlight
-      --      tail = 'TabLine',           -- tail element highlight
-      --    },
-      --    nerdfont      = true,
-      --    lualine_theme = nil,
-      --    tab_name = {
-      --      name_fallback = function(tabid) return tabid end,
-      --    },
-      --    buf_name = {
-      --      mode = 'unique', -- or 'relative', 'tail', 'shorten'
-      --    },
-      --  },
     })
   else
     vim.notify(
