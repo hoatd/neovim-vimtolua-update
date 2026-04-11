2429cc1c0e2732eeda5ecc1c7d140161f0ad7698
Duy Hoa Tran <hoatranduy@gmail.com>
2026-04-11 09:03:11 +0200

    Update copilot config

--- nvim/lua/config/copilot.lua
+++ nvim/lua/config/copilot.lua
@@ -1,42 +1,45 @@
 local M = {}
 
 function M.setup()
+  -- vim.g.copilot_enterprise_uri = "https://my-company.ghe.com"
+  -- vim.g.copilot_workspace_folders = {
+  --   vim.fn.expand("~/Projects/my-project"),
+  --   vim.fn.expand("~/Projects/another-project"),
+  -- }
   vim.g.copilot_no_tab_map = true
 
   local map = vim.keymap.set
   local opts = { silent = true, noremap = true }
 
   map("i", "<M-l>", function()
-    vim.fn.feedkeys(vim.fn["copilot#Accept"]("\r"), "n")
-  end, vim.tbl_extend("force", opts, { desc = "Copilot accepts ghost" }))
-
+    local accept = vim.fn["copilot#Accept"]("")
+    if accept ~= "" then
+      vim.api.nvim_feedkeys(accept, "n", true)
+    end
+  end, vim.tbl_extend("force", opts, { desc = "Copilot accept suggestion" }))
   map(
     "i",
     "<M-ü>",
     "<Plug>(copilot-next)",
-    vim.tbl_extend("force", opts, { desc = "Copilot next ghost" })
+    vim.tbl_extend("force", opts, { desc = "Copilot next suggestion" })
   )
-
   map(
     "i",
     "<M-+>",
     "<Plug>(copilot-previous)",
-    vim.tbl_extend("force", opts, { desc = "Copilot previous ghost" })
+    vim.tbl_extend("force", opts, { desc = "Copilot previous suggestion" })
   )
-
-  -- Dismiss
   map(
     "i",
     "<C-]>",
     "<Plug>(copilot-dismiss)",
-    vim.tbl_extend("force", opts, { desc = "Copilot dismiss ghost" })
+    vim.tbl_extend("force", opts, { desc = "Copilot dismiss suggestion" })
   )
-
   map(
     "n",
-    "<M-CR>",
+    "<M-p>",
     ":Copilot panel<CR>",
-    vim.tbl_extend("force", opts, { desc = "Copilot opens panel" })
+    vim.tbl_extend("force", opts, { desc = "Copilot open panel" })
   )
 end
 
