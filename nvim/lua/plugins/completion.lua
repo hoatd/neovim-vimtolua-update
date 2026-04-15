1e18137b397f18d782f6434df317e3bc97a7705c
Duy Hoa Tran <hoatranduy@gmail.com>
2026-04-15 07:09:47 +0200

    Revise cmp navigation keys

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -80,33 +80,42 @@ return {
         mapping = cmp.mapping.preset.insert({
           ["<C-n>"] = cmp.mapping.select_next_item(),
           ["<C-p>"] = cmp.mapping.select_prev_item(),
-          ["<C-d>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = false }),
 
-          -- Tab / S-Tab: snippet jump + completion navigation (uncomment to enable)
-          -- ["<Tab>"] = cmp.mapping(function(fallback)
-          --   if cmp.visible() then
-          --     cmp.select_next_item()
-          --   elseif luasnip.expandable() then
-          --     luasnip.expand()
-          --   elseif luasnip.locally_jumpable(1) then
-          --     luasnip.jump(1)
-          --   else
-          --     fallback()
-          --   end
-          -- end, { "i", "s" }),
-          -- ["<S-Tab>"] = cmp.mapping(function(fallback)
-          --   if cmp.visible() then
-          --     cmp.select_prev_item()
-          --   elseif luasnip.locally_jumpable(-1) then
-          --     luasnip.jump(-1)
-          --   else
-          --     fallback()
-          --   end
-          -- end, { "i", "s" }),
+          -- Accept ghost text suggestion (top candidate) without navigating
+          ["<C-Right>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.confirm({ select = true })
+            else
+              fallback()
+            end
+          end, { "i" }),
+
+          -- Tab / S-Tab: snippet jump + completion navigation
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.locally_jumpable(1) then
+              luasnip.jump(1)
+            elseif luasnip.expandable() then
+              luasnip.expand()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
         }),
 
         experimental = {
@@ -149,7 +158,12 @@ return {
   { "hrsh7th/cmp-buffer", enabled = true, lazy = true },
   { "hrsh7th/cmp-path", enabled = true, lazy = true },
   { "hrsh7th/cmp-cmdline", enabled = true, lazy = true },
-  { "petertriho/cmp-git", enabled = true, lazy = true, dependencies = { "nvim-lua/plenary.nvim" } },
+  {
+    "petertriho/cmp-git",
+    enabled = true,
+    lazy = true,
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
   { "saadparwaiz1/cmp_luasnip", enabled = true, lazy = true },
   { "onsails/lspkind.nvim", enabled = true, lazy = true },
 }
