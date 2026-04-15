1076fcc5db196c1345414c4fdba83c48cd0449f8
Duy Hoa Tran <hoatranduy@gmail.com>
2026-04-15 12:42:06 +0200

    Opencode using snacks terminal

--- nvim/lua/plugins/ai/opencode.lua
+++ nvim/lua/plugins/ai/opencode.lua
@@ -41,16 +41,41 @@ return {
       },
     },
     config = function()
-      require("opencode")
+      local opencode_cmd = "opencode --port"
+      ---@type snacks.terminal.Opts
+      local snacks_terminal_opts = {
+        win = {
+          position = "right",
+          enter = false,
+          on_win = function(win)
+            require("opencode.terminal").setup(win.win)
+          end,
+        },
+      }
       ---@type opencode.Opts
       vim.g.opencode_opts = {
         lsp = {
           enabled = true,
         },
+        server = {
+          start = function()
+            require("snacks.terminal").open(opencode_cmd, snacks_terminal_opts)
+          end,
+          stop = function()
+            require("snacks.terminal")
+              .get(opencode_cmd, snacks_terminal_opts)
+              :close()
+          end,
+          toggle = function()
+            require("snacks.terminal").toggle(
+              opencode_cmd,
+              snacks_terminal_opts
+            )
+          end,
+        },
       }
 
       vim.o.autoread = true -- Required for `opts.events.reload`
-
 
       vim.keymap.set({ "n", "x" }, "<C-a>", function()
         require("opencode").ask("@this: ", { submit = true })
