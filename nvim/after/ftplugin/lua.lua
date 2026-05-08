de51919b609aae05b4bdee3a9c1b30970d678485
Duy Hoa Tran <hoatranduy@gmail.com>
2026-05-08 14:35:36 +0200

    Set colorcolum 80 for lua and vim

---
+++ nvim/after/ftplugin/lua.lua
@@ -0,0 +1,3 @@
+vim.opt_local.colorcolumn = {
+  "80", -- Common limit for Lua style/readability
+}
---
+++ nvim/after/ftplugin/vim.lua
@@ -0,0 +1,3 @@
+vim.opt_local.colorcolumn = {
+  "80", -- Common limit for Vim script style/readability
+}
